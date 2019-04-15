cd8d04bf55880acc522d9c3e32540c29626f1896
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-15 06:30:19 -0400

    Update travis-docker.R

--- travis-docker.R
+++ travis-docker.R
@@ -3,8 +3,8 @@
 # Note that BiocCheck requires directory name to match package name.
 setwd("/acidtest")
 
-# devtools::check(pkg = ".", document = FALSE)
-rcmdcheck::rcmdcheck(path = ".")
+# devtools::check(pkg = ".", document = FALSE, manual = FALSE)
+rcmdcheck::rcmdcheck(path = ".", args = "--no-manual")
 BiocCheck::BiocCheck(package = ".")
-lintr::lint_package(path = ".")
+# lintr::lint_package(path = ".")
 covr::report()
