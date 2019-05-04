a3c77bbc0cf180b2f81a08e9e219f432f819ade3
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 16:54:33 -0400

    Update Docker

--- travis-docker.R
+++ travis-docker.R
@@ -1,4 +1,24 @@
 Sys.setenv(TZ = "America/New_York")
-rcmdcheck::rcmdcheck(args = "--no-manual")
-BiocCheck::BiocCheck()
-lintr::lint_package()
+sessioninfo::session_info()
+rcmdcheck::rcmdcheck(
+    path = ".",
+    args = c(
+        "--no-build-vignettes",
+        "--no-manual",
+        "--no-vignettes",
+        "--timings"
+    ),
+    build_args = c(
+        "--no-build-vignettes",
+        "--no-manual"
+    ),
+    error_on = "error"
+)
+
+if (packageVersion("base") >= "3.6") {
+    BiocCheck::BiocCheck(
+        package = ".",
+        `quit-with-status` = TRUE
+    )
+    lintr::lint_package()
+}
