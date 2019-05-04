39e0e70f19f7e01f2c2e04b132bc72cc4ff2d5b8
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 08:30:55 -0400

    Improve docker script

--- travis-docker.sh
+++ travis-docker.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail
+
 pkg="acidtest"
 image="acidgenomics/basejump"
+
 docker pull "$image"
 docker run -ti --volume="${PWD}:/${pkg}" --workdir="/${pkg}" "$image" Rscript -e 'source("travis-docker.R")'
