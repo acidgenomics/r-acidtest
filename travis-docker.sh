436d6a94885de3a64a11d43771fc860e92610aa0
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 08:53:14 -0400

    Improve Travis config to allow versioned docker containers

--- travis-docker.sh
+++ travis-docker.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-pkg="acidtest"
-image="acidgenomics/basejump"
+if [[ -z "${tag:-}" ]]
+then
+    tag="latest"
+fi
+
+image="acidgenomics/basejump:${tag}"
+package="$(basename "$TRAVIS_BUILD_DIR")"
 
 docker pull "$image"
-docker run -ti --volume="${PWD}:/${pkg}" --workdir="/${pkg}" "$image" Rscript -e 'source("travis-docker.R")'
+docker run -ti --volume="${PWD}:/${package}" --workdir="/${package}" "$image" Rscript -e 'source("travis-docker.R")'
