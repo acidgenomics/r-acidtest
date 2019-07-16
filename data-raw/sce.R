a5e115251a2c515fba5c89a40a158f2cc5163077
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-16 12:07:44 -0400

    Rework sce/seurat objects

--- data-raw/sce.R
+++ data-raw/sce.R
@@ -1,75 +1,16 @@
-# Gene-level SingleCellExperiment example.
+# Create SingleCellExperiment object from Seurat.
 # Updated 2019-07-16.
-
-# Splatter params are derived from:
-# https://github.com/mikelove/zinbwave-deseq2/blob/master/zinbwave-deseq2.knit.md
 
 library(usethis)
 library(pryr)
-library(SingleCellExperiment)
-library(basejump)
-library(splatter)
+library(pointillism)  # 0.3.2
 
-# Restrict to 2 MB.
+# Restrict to 1 MB.
 # Use `pryr::object_size()` instead of `utils::object.size()`.
-limit <- structure(2e6, class = "object_size")
+limit <- structure(1e6, class = "object_size")
 
-organism <- "Homo sapiens"
-release <- 92L
-
-# Use splatter to generate an example dataset with simulated counts.
-# Note: These DE params are natural log scale.
-params <- newSplatParams() %>%
-    setParam(name = "batchCells", value = 100L) %>%
-    setParam(name = "nGenes", value = 500L) %>%
-    setParam(name = "de.facLoc", value = 1L) %>%
-    setParam(name = "de.facScale", value = 0.25) %>%
-    # Add more dropout (to test zinbwave weights and DE).
-    setParam(name = "dropout.type", value = "experiment") %>%
-    setParam(name = "dropout.mid", value = 3L)
-sce <- splatSimulate(
-    params = params,
-    group.prob = c(0.5, 0.5),
-    method = "groups"
-)
-
-# Sanitize the dimnames into camel case.
-sce <- camel(sce, rownames = TRUE, colnames = TRUE)
-
-# Prepare column data.
-colData(sce) <- camel(colData(sce))
-# Add `sampleID` column. Note that `sampleName` is recommended, but if it is
-# not defined, it should be generated from the `sampleID` automatically.
-sce$sampleID <- factor(gsub("group", "sample", camel(sce$group)))
-sce$batch <- NULL
-sce$cell <- NULL
-sce$group <- NULL
-
-# Pad the zeros in rows and columns.
-# Note that this needs to come after setting up `colData`, otherwise will
-# error because `sampleID` column is not defined.
-sce <- autopadZeros(sce)
-
-# Just slot the raw counts, as a sparse matrix.
-counts <- counts(sce)
-counts <- as(counts, "sparseMatrix")
-assays(sce) <- list(counts = counts)
-
-# Prepare row data.
-rowRanges <- makeGRangesFromEnsembl(organism, release = release)
-rowRanges <- rowRanges[
-    i = seq_len(nrow(sce)),
-    j = c("geneID", "geneName", "geneBiotype", "broadClass", "entrezID")
-]
-# Relevel the factor columns, to save disk space.
-rowRanges <- relevelRowRanges(rowRanges)
-# Note that we're keeping the original rownames from dds_small, and they won't
-# match the `geneID` column in rowRanges. This is intentional, for unit testing.
-names(rowRanges) <- rownames(sce)
-rowRanges(sce) <- rowRanges
-
-# Stash minimal metadata.
-metadata(sce) <- list(date = Sys.Date())
+data(seurat, package = "acidtest")
+sce <- as(seurat, "SingleCellExperiment")
 
 # Report the size of each slot in bytes.
 vapply(
@@ -80,5 +21,3 @@ vapply(
 object_size(sce)
 stopifnot(object_size(sce) < limit)
 validObject(sce)
-
-use_data(sce, compress = "xz", overwrite = TRUE)
--- data-raw/seurat.R
+++ data-raw/seurat.R
@@ -9,6 +9,13 @@ library(Seurat)  # 3.0
 library(bcbioSingleCell)
 library(tidyverse)
 
+# ```sh
+# > source ~/.virtualenvs/reticulate/bin/activate
+# > pip install --upgrade pip
+# > pip install umap-learn
+# > deactivate
+# ```
+
 # Check and make sure Python umap-learn is accessible to run UMAP.
 # We're using this in the `Seurat::RunUMAP()` call below.
 #
@@ -26,34 +33,13 @@ use_virtualenv(virtualenv = "reticulate", required = TRUE)
 
 py_config()
 
-# [Azure VM]
+# [Azure]
 # python:         /home/mike/.virtualenvs/reticulate/bin/python
-# libpython:      /usr/local/lib/libpython3.7m.so
-# pythonhome:     /usr/local:/usr/local
-# version:        3.7.3 (default, Apr 11 2019, 16:32:38)  [GCC 4.8.5 20150623 (Red Hat 4.8.5-36)]
+# libpython:      /usr/local/koopa/cellar/python/3.7.3/lib/libpython3.7m.so
+# pythonhome:     /usr/local/koopa/cellar/python/3.7.3:/usr/local/koopa/cellar/python/3.7.3
+# version:        3.7.3 (default, Jun 24 2019, 13:12:54)  [GCC 4.8.5 20150623 (Red Hat 4.8.5-36)]
 # numpy:          /home/mike/.virtualenvs/reticulate/lib/python3.7/site-packages/numpy
-# numpy_version:  1.16.2
-#
-# python versions found:
-#  /usr/bin/python
-#  /usr/local/bin/python3
-#  /home/mike/.virtualenvs/reticulate/bin/python
-
-# [macOS]
-# python:         /usr/local/opt/python/libexec/bin/python
-# libpython:      /usr/local/opt/python/Frameworks/Python.framework/Versions/3.7/lib/python3.7/config-3.7m-darwin/libpython3.7.dylib
-# pythonhome:     /usr/local/opt/python/Frameworks/Python.framework/Versions/3.7:/usr/local/opt/python/Frameworks/Python.framework/Versions/3.7
-# version:        3.7.2 (default, Feb 12 2019, 08:15:36)  [Clang 10.0.0 (clang-1000.11.45.5)]
-# numpy:          /usr/local/lib/python3.7/site-packages/numpy
-# numpy_version:  1.16.2
-#
-# python versions found:
-#  /usr/local/opt/python/libexec/bin/python
-#  /usr/bin/python
-#  /usr/local/bin/python
-#  /usr/local/bin/python3
-#  /Users/mike/anaconda3/bin/python
-#  /Users/mike/.virtualenvs/reticulate/bin/python
+# numpy_version:  1.16.4
 
 stopifnot(py_module_available(module = "umap"))
 
@@ -73,19 +59,22 @@ seurat <- pbmc_small
 # Alternatively, can use `features` here instead.
 seurat <- RunUMAP(seurat, dims = seq_len(10L))
 
+# Slot row ranges into the Seurat object.
+rowRanges <- makeGRangesFromEnsembl(
+    organism = "Homo sapiens",
+    genomeBuild = "GRCh37"
+)
+x <- rownames(seurat)
+table <- make.unique(as.character(rowRanges$geneName))
+names(rowRanges) <- table
+stopifnot(all(x %in% table))
+which <- match(x = x, table = table)
+rowRanges <- rowRanges[which] %>% relevelRowRanges()
+stopifnot(object_size(rowRanges) < limit)
+rowRanges(seurat) <- rowRanges
+
 object_size(seurat)
 stopifnot(object_size(seurat) < limit)
 validObject(seurat)
-
-# The pbmc_small gene symbols map to GRCh37.
-gr <- makeGRangesFromEnsembl(organism = "Homo sapiens", genomeBuild = "GRCh37")
-x <- rownames(seurat)
-table <- make.unique(as.character(gr$geneName))
-names(gr) <- table
-stopifnot(all(x %in% table))
-which <- match(x = x, table = table)
-gr <- gr[which]
-# Note that `rowRanges` method is defined by pointillism.
-rowRanges(seurat) <- gr
 
 usethis::use_data(seurat, compress = "xz", overwrite = TRUE)
