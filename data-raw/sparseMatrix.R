882d7f9c7226108e67ee8b5966a77370f9f3b03c
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-21 07:18:11 -0400

    Add dimnames

--- data-raw/sparseMatrix.R
+++ data-raw/sparseMatrix.R
@@ -3,5 +3,16 @@ library(Matrix)
 i <- c(1L, 3L:8L)
 j <- c(2L, 9L, 6L:10L)
 x <- 7 * (1:7)
-sparseMatrix <- sparseMatrix(i = i, j = j, x = x)
+nrow <- 8L
+ncol <- 10L
+sparseMatrix <- sparseMatrix(
+    i = i,
+    j = j,
+    x = x,
+    dims = c(nrow, ncol),
+    dimnames = list(
+        sprintf("gene%02d", seq_len(nrow)),
+        sprintf("sample%02d", seq_len(ncol))
+    )
+)
 use_data(sparseMatrix, compress = "xz", overwrite = TRUE)
