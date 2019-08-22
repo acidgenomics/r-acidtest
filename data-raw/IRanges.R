a750cd1e24bda137abb0ce9b341a200c26ab5539
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-22 17:06:30 -0400

    Improve lints

--- data-raw/IRanges.R
+++ data-raw/IRanges.R
@@ -3,7 +3,7 @@ library(IRanges)
 IRanges <- IRanges(
     c(1L, 10L, 20L),
     width = 5L,
-    names = letters[1L:3L]
+    names = letters[seq_len(3L)]
 )
 mcols(IRanges) <- DataFrame(score = seq(3L))
 use_data(IRanges, compress = "xz", overwrite = TRUE)
