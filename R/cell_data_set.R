e0ff655f7d830f64bb79eccce1d7c932f69392df
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-30 21:35:30 -0400

    Update package for Bioconductor 3.10

--- R/cell_data_set.R
+++ R/cell_data_set.R
@@ -2,10 +2,14 @@
 #'
 #' Example monocle3 `cell_data_set` object.
 #'
-#' @note Updated 2019-08-02.
+#' @note Updated 2019-10-30.
 #'
 #' @examples
-#' if (requireNamespace("monocle3", quietly = TRUE)) {
+#' ## Not currently working on Bioconductor 3.10.
+#' if (
+#'     requireNamespace("monocle3", quietly = TRUE) &&
+#'     packageVersion("SingleCellExperiment") < "1.8"
+#' ) {
 #'     data(cell_data_set)
 #'     class(cell_data_set)
 #'     cell_data_set
