7b9ff3312cd9ac7c325a781833910e8e5d19ed2b
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-19 23:11:05 -0500

    Migrate example data from transformer

---
+++ R/joins.R
@@ -0,0 +1,17 @@
+#' @inherit dplyr::band_instruments description return title
+#' @keywords internal
+#' @seealso `help(topic = "band_instruments", package = "dplyr")`
+#' @examples
+#' data(band_instruments)
+#' band_instruments
+"band_instruments"
+
+
+
+#' @inherit dplyr::band_members description return title
+#' @keywords internal
+#' @seealso `help(topic = "band_members", package = "dplyr")`
+#' @examples
+#' data(band_members)
+#' band_members
+"band_members"
