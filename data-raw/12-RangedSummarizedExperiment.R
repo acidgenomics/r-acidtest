ff95f58540d41379f78b5a5a071c2ff605aa94da
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-14 17:07:43 -0500

    Resave

--- data-raw/12-RangedSummarizedExperiment.R
+++ data-raw/12-RangedSummarizedExperiment.R
@@ -1,54 +1,50 @@
 library(usethis)
-library(pryr)                  # 0.1.4
-library(SummarizedExperiment)  # 1.16.1
-library(basejump)              # 0.12.4
-library(DESeq2)                # 1.26.0
-
+library(pryr)
+library(SummarizedExperiment)
+library(basejump)
+library(DESeq2)
 ## Restrict to 1.5 MB.
 ## Use `pryr::object_size()` instead of `utils::object.size()`.
 limit <- structure(1.5e6, class = "object_size")
-
 organism <- "Homo sapiens"
 release <- 99L
-
 ## Generate example DESeqDataSet using DESeq2.
 ## Note that we're using simulated counts here.
 dds <- makeExampleDESeqDataSet(n = 500L, m = 12L, betaSD = 1L)
 object_size(dds)
 stopifnot(object_size(dds) < limit)
 validObject(dds)
-
 ## Coerce to RangedSummarizedExperiment.
 ## Need to change rows to actual gene identifiers here, and slot colData.
 rse <- as(dds, "RangedSummarizedExperiment")
 object_size(rse)
 stopifnot(object_size(rse) < limit)
-
 ## Pad the dimnames so they sort correctly.
 rse <- autopadZeros(rse, rownames = TRUE)
-
 ## Row data. Include real `geneId`, `geneName` columns to test mapping.
 rowRanges <- makeGRangesFromEnsembl(organism = organism, release = release)
 ## Subset to match the number of rows in the example.
 rowRanges <- rowRanges[seq_len(nrow(rse)), ]
+colnames(mcols(rowRanges)) <-
+    camelCase(
+        object = colnames(mcols(rowRanges)),
+        strict = TRUE
+    )
 ## Relevel the factor columns, to save disk space.
 rowRanges <- droplevels(rowRanges)
 ## Note that we're keeping the original rownames from dds_small, and they won't
 ## match the `geneId` column in rowRanges. This is intentional, for unit tests.
 names(rowRanges) <- rownames(rse)
 rowRanges(rse) <- rowRanges
-
 ## Metadata.
 ## Stash the date.
 metadata(rse)[["date"]] <- Sys.Date()
 ## Define the interesting groups.
 interestingGroups(rse) <- "condition"
-
 ## Size check.
 lapply(coerceS4ToList(rse), object_size)
 object_size(rse)
 stopifnot(object_size(rse) < limit)
 validObject(rse)
-
 RangedSummarizedExperiment <- rse
 use_data(RangedSummarizedExperiment, compress = "xz", overwrite = TRUE)
--- data-raw/13-SingleCellExperiment.R
+++ data-raw/13-SingleCellExperiment.R
@@ -1,23 +1,25 @@
-## Gene-level SingleCellExperiment example.
-## Updated 2020-03-16.
-##
+#' Gene-level SingleCellExperiment example
+#'
+#' @note Updated 2020-03-16.
+#' @noRd
+#
+#' @details
 ## Splatter params are derived from:
 ## https://github.com/mikelove/zinbwave-deseq2/blob/master/
 ##     zinbwave-deseq2.knit.md
+NULL
 
 library(usethis)
-library(pryr)                  # 0.1.4
-library(SingleCellExperiment)  # 1.8.0
-library(basejump)              # 0.12.4
-library(splatter)              # 1.10.1
+library(pryr)
+library(SingleCellExperiment)
+library(basejump)
+library(splatter)
 
 ## Restrict to 2 MB.
 ## Use `pryr::object_size()` instead of `utils::object.size()`.
 limit <- structure(2e6, class = "object_size")
-
 organism <- "Homo sapiens"
 release <- 99L
-
 ## Use splatter to generate an example dataset with simulated counts.
 ## Note: These DE params are natural log scale.
 params <- newSplatParams() %>%
@@ -33,43 +35,40 @@ sce <- splatSimulate(
     group.prob = c(0.5, 0.5),
     method = "groups"
 )
-
 ## Sanitize the dimnames into camel case.
-sce <- camelCase(sce, rownames = TRUE, colnames = TRUE)
+sce <- camelCase(sce, rownames = TRUE, colnames = TRUE, strict = TRUE)
 colData(sce) <- camelCase(colData(sce))
-
 ## Prepare column data.
 colData(sce) <- DataFrame(
     "sampleId" = factor(gsub("group", "sample", camelCase(sce$group))),
     row.names = colnames(sce)
 )
-
 ## Pad the zeros in rows and columns.
 sce <- autopadZeros(sce, rownames = TRUE, colnames = TRUE)
-
 ## Just slot the raw counts, as a sparse matrix.
 counts <- counts(sce)
 counts <- as(counts, "sparseMatrix")
 assays(sce) <- list(counts = counts)
-
 ## Prepare row data.
 rowRanges <- makeGRangesFromEnsembl(organism, release = release)
 rowRanges <- rowRanges[seq_len(nrow(sce)), ]
+colnames(mcols(rowRanges)) <-
+    camelCase(
+        object = colnames(mcols(rowRanges)),
+        strict = TRUE
+    )
 ## Relevel the factor columns, to save disk space.
 rowRanges <- droplevels(rowRanges)
 ## Note that we're keeping the original rownames from dds_small, and they won't
 ## match the `geneId` column in rowRanges. This is intentional, for unit tests.
 names(rowRanges) <- rownames(sce)
 rowRanges(sce) <- rowRanges
-
 ## Stash minimal metadata.
 metadata(sce) <- list(date = Sys.Date())
-
 ## Report the size of each slot in bytes.
 lapply(coerceS4ToList(sce), object_size)
 object_size(sce)
 stopifnot(object_size(sce) < limit)
 validObject(sce)
-
 SingleCellExperiment <- sce
 use_data(SingleCellExperiment, compress = "xz", overwrite = TRUE)
