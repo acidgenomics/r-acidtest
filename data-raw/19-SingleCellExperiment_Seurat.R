1f967e684675d6ba01db581c2248091e42b85566
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-12 09:02:45 -0500

    Resave example

--- data-raw/19-SingleCellExperiment_Seurat.R
+++ data-raw/19-SingleCellExperiment_Seurat.R
@@ -1,4 +1,4 @@
-## NOTE Can simplify internal code here following pointillism update.
+## NOTE Can simplify code here following pointillism update.
 suppressPackageStartupMessages({
     library(devtools)
     library(usethis)
@@ -8,17 +8,18 @@ suppressPackageStartupMessages({
 load_all()
 limit <- structure(1e6, class = "object_size")
 data(Seurat)
-## FIXME Consider making this step coerce rowRanges mcols colnames to strict
-## lower camel case.
-## FIXME Consider making this step coerce colData colnames to strict lower
-## camel case.
+## NOTE Consider reworking coercion method to use strict lower camel case
+## for names in colData and rowData.
 sce <- as(Seurat, "SingleCellExperiment")
+colnames(mcols(rowRanges(sce)))
+##  [1] "broadClass"                "entrezId"
+##  [3] "geneBiotype"               "geneId"
+##  [5] "geneName"                  "seqCoordSystem"
+##  [7] "vst.mean"                  "vst.variance"
+##  [9] "vst.variance.expected"     "vst.variance.standardized"
+## [11] "vst.variable"
 colnames(mcols(rowRanges(sce))) <-
-    gsub(
-        pattern = "Id$",
-        replacement = "ID",
-        x = colnames(mcols(rowRanges(sce)))
-    )
+    camelCase(colnames(mcols(rowRanges(sce))), strict = TRUE)
 sce <- convertSymbolsToGenes(sce)
 colnames(mcols(rowRanges(sce))) <-
     camelCase(
@@ -32,30 +33,28 @@ colnames(mcols(rowRanges(sce)))
 ##  [7] "vstMean"                 "vstVariance"
 ##  [9] "vstVarianceExpected"     "vstVarianceStandardized"
 ## [11] "vstVariable"
-colnames(colData(sce)) <-
-    camelCase(
-        object = colnames(colData(sce)),
-        strict = TRUE
-    )
 colnames(colData(sce))
-## [1] "origIdent"    "nCountRna"    "nFeatureRna"  "rnaSnnRes0x8"
-## [5] "letterIdents" "groups"       "rnaSnnRes1"   "ident"
-## Assays.
+## [1] "orig.ident"      "nCount_RNA"      "nFeature_RNA"    "RNA_snn_res.0.8"
+## [5] "letter.idents"   "groups"          "RNA_snn_res.1"   "ident"
+colnames(colData(sce)) <-
+    camelCase(colnames(colData(sce)), strict = TRUE)
+colnames(colData(sce))
+## [1] "origIdent"    "nCountRna"    "nFeatureRna"  "rnaSnnRes0x8" "letterIdents"
+## [6] "groups"       "rnaSnnRes1"   "ident"
 stopifnot(identical(assayNames(sce), c("counts", "logcounts")))
-## > assays(sce) <- assays(sce)["counts"]
 ## Dimensionality reduction.
-## FIXME Coercion method should automatically coerce dim names to lower
-## camel case.
+## NOTE Coercion method should automatically coerce dim names to lower
+## camel case?
 stopifnot(identical(reducedDimNames(sce), c("PCA", "TSNE", "UMAP")))
 reducedDims(sce) <- reducedDims(sce)["UMAP"]
-reducedDimNames(sce) <- camelCase(reducedDimNames(sce))
+reducedDimNames(sce) <- camelCase(reducedDimNames(sce), strict = TRUE)
 ## Column data.
 cd <- colData(sce) %>% .[, "groups", drop = FALSE]
-cd$sampleId <- factor(gsub("g", "sample", camelCase(cd$groups)))
+cd[["sampleId"]] <- factor(gsub("g", "sample", camelCase(cd[["groups"]])))
 cd <- cd[, "sampleId", drop = FALSE]
 colData(sce) <- cd
 ## Metadata.
-metadata(sce) <- list(date = Sys.Date())
+metadata(sce) <- list("date" = Sys.Date())
 ## Row ranges.
 ## Drop "vst*" columns.
 keep <- !grepl(pattern = "^vst", x = colnames(mcols(rowRanges(sce))))
