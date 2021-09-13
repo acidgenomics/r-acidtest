bb2870c36a1d9fe36c139a38527f603244838047
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-13 09:37:52 -0400

    Resave basic SE, with better metadata

--- data-raw/12-SummarizedExperiment.R
+++ data-raw/12-SummarizedExperiment.R
@@ -2,18 +2,6 @@ suppressPackageStartupMessages({
     library(usethis)
     library(SummarizedExperiment)
 })
-nrow <- 50L
-ncol <- 4L
-assay <- matrix(
-    data = runif(nrow * ncol, 1L, 1e4L),
-    nrow = nrow,
-    ncol = ncol,
-    dimnames = list(
-        sprintf("gene%02d", seq_len(nrow)),
-        sprintf("sample%02d", seq_len(ncol))
-    )
-)
-assay <- round(assay)
-mode(assay) <- "integer"
-SummarizedExperiment <- SummarizedExperiment(assays = SimpleList(assay))
+data(RangedSummarizedExperiment)
+SummarizedExperiment <- as(RangedSummarizedExperiment, "SummarizedExperiment")
 use_data(SummarizedExperiment, compress = "xz", overwrite = TRUE)
