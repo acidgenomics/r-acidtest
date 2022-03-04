6d8263506bb60250977261b8375ebc91307464d4
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-04 16:20:16 -0500

    Resave Seurat object

--- data-raw/18-Seurat.R
+++ data-raw/18-Seurat.R
@@ -1,21 +1,10 @@
-## FIXME Need to resave Seurat example object. Now seeing this error:
-##
-## Loading required package: SeuratObject
-## Warning in library(package, lib.loc = lib.loc, character.only = TRUE, logical.return = TRUE,  :
-##   there is no package called ‘SeuratObject’
-## Calls: <Anonymous> ... tryCatchList -> tryCatchOne -> doTryCatch -> library
-## Error in .requirePackage(package) :
-##   unable to find required package ‘SeuratObject’
-## Calls: <Anonymous> ... .findInheritedMethods -> getClass -> getClassDef -> .requirePackage
-
-
-
 suppressPackageStartupMessages({
     library(usethis)
     library(lobstr)
-    library(basejump)
-    library(Seurat)
-    library(pointillism)
+    library(basejump)      # 0.15.0.9000
+    library(Seurat)        # 4.1.0
+    library(SeuratObject)  # 4.0.4
+    library(pointillism)   # 0.6.0.9000
 })
 ## Restrict object size to 1 MB.
 limit <- structure(1e6, class = "object_size")
@@ -25,14 +14,10 @@ data(pbmc_small, package = "SeuratObject")
 ## https://github.com/satijalab/seurat/wiki
 Seurat <- UpdateSeuratObject(pbmc_small)
 stopifnot(obj_size(Seurat) < limit)
-## Add UMAP dimensional reduction to example object.
-## Alternatively, can use `features` here instead.
-## NOTE This step can crash R when using `umap.method = "umap-learn` with
-## umap-learn v0.5.1.
-Seurat <- RunUMAP(
-    object = Seurat,
-    dims = seq_len(10L)
-)
+## Add UMAP dimensional reduction to example object. Alternatively, can use
+## `features` here instead. This step can crash R when using
+## `umap.method = "umap-learn` with umap-learn v0.5.1.
+Seurat <- RunUMAP(object = Seurat, dims = seq_len(10L))
 ## Slot row ranges into the Seurat object.
 rowRanges <- makeGRangesFromEnsembl(
     organism = "Homo sapiens",
