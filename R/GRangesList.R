27ebe344694d83178813de2626ddfdd219b5a86a
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-08 08:49:37 -0500

    Add GRangesList

--- R/GRangesList.R
+++ R/GRangesList.R
@@ -1,1 +1,16 @@
-## FIXME Add this.
+#' S4 genomic ranges list
+#'
+#' Example Bioconductor S4 `GRangesList` object.
+#'
+#' @note Updated 2023-11-08.
+#' @usage data(GRangesList)
+#' @return `GRangesList`.
+#'
+#' @examples
+#' if (requireNamespace("GenomicRanges", quietly = TRUE)) {
+#'     data(GRangesList)
+#'     class(GRangesList)
+#'     is(GRangesList, "GRangesList")
+#'     print(GRangesList)
+#' }
+"GRangesList"
